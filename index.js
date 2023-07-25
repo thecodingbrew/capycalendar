670ada191111327f815f8442391dfd9ce29e0a93
vladislavaSim <simonova.vladislava@gmail.com>
2023-07-25 10:50:05 +0200

    Add leap/non-leap year handling

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const monthDaysCalculator = {
     if ([4, 6, 9, 11].includes(month)) {
       return 30;
     }
-
+    if (month === 2) {
+        if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+          return 29; // leap year
+        } else {
+          return 28; // non-leap year
+        }
+      }
     // other months have 31 days
     return 31;
   }
 }
- console.log(monthDaysCalculator.getDaysInMonth(null, 6)); 
+ console.log(monthDaysCalculator.getDaysInMonth(2021, 2)); 
