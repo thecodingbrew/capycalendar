e8b5f368672d5545c7355ab9900d4b668a89db7e
vladislavaSim <simonova.vladislava@gmail.com>
2023-07-26 17:18:45 +0200

    Add checking if year and month are integer numbers. If not, throw error

--- monthDaysCalculator.js
+++ monthDaysCalculator.js
@@ -1,6 +1,9 @@
 const monthDaysCalculator = {
   getDaysInMonth: (year, month) => {
     //error if month input is incorrect
+    if(!year.isInteger() || !month.isInteger()) {
+      throw new Error('Error: year and month must be integer numbers')
+    }
     if (month < 1 || month > 12) {
         throw new Error("Invalid month. Month must be between 1 and 12.");
       }
@@ -19,11 +22,16 @@ const monthDaysCalculator = {
     return 31;
   },
   checkIfLeapYear: (year) => {
-    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
-      return true; // leap year
+    if(year.isInteger()) {
+      if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+        return true; // leap year
+      } else {
+        return false; // non-leap year
+      }
     } else {
-      return false; // non-leap year
+      throw new Error('Error: year must be integer number')
     }
+    
   }
 };
 module.exports = monthDaysCalculator;
